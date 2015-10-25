fd0cee8a7cc8b1ec3874fdca987f64f4840ce8cf
justin talbott <justin@waymondo.com>
2015-10-25 19:25:58 -0400

    Gemfile.lock update, syntax cleanup

--- lib/turboboost/form_helper.rb
+++ lib/turboboost/form_helper.rb
@@ -20,7 +20,7 @@ module Turboboost
       form_for_without_data_turboboost(record_or_name_or_array, *(args << options), &proc)
     end
 
-    def form_tag_with_data_turboboost(url_for_options={}, *args, &proc)
+    def form_tag_with_data_turboboost(url_for_options = {}, *args, &proc)
       options = args.extract_options!
 
       if options.key?(:turboboost) && options.delete(:turboboost)
